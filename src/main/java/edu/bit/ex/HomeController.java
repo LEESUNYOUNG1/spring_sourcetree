b93a0d0c176177d2053a7b6fc15a1f3d1a9e0f70
DESKTOP-RTCC32Q\bit <jazz9840@naver.com>
2020-08-24 14:08:03 +0900

    test2-commit

--- src/main/java/edu/bit/ex/HomeController.java
+++ src/main/java/edu/bit/ex/HomeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
+//test-2
 //test-1
 @Controller
 public class HomeController {
